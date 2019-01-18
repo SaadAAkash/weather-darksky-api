bcb329b506cc9cf836ec33a63cbc98b18e860a0a
-1 <sa.akash0129@gmail.com>
2019-01-18 15:16:02 +0600

    android project added

---
+++ app/build/generated/source/buildConfig/debug/com/saad/weatherwizard/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.saad.weatherwizard;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.saad.weatherwizard";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 7;
+  public static final String VERSION_NAME = "2.0.2";
+  // Fields from default config.
+  public static final String API_KEY = "4c36ef049f94261e8827bea9982bccc1";
+}
